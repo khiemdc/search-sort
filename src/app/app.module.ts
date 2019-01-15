33cf9bf1e9178415bb3bb28860e7687110ef73b8
khiemdc <khiemle.dc@gmail.com>
2019-01-15 13:26:22 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Pipe, PipeTransform } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './category.component';
+import { CategoryPipe } from './category.pipe';
+import { OrderByPipe } from './orderby.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    CategoryPipe, 
+    OrderByPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
-})
+  })
 export class AppModule { }
